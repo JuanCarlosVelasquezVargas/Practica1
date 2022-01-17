cedeca4a4d0d70e205d6f7dd5aae7ab733f1bbae
[JuanCarlosVelásquezVargas] <[juanvelasquez7914@hotmail.com]>
2022-01-17 12:24:14 -0500

    Commit Clase 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Practica1';
+
+  nombreEstudiante: string = 'Juan Velásquez';
+  edadEstudiante: number = 33;
+  horaActual: Date;
+  colors: IColor[] = [
+    {redSocial: 'Whatsapp', color: '#25D366'},
+    {redSocial: 'Facebook', color: '#4267B2'},
+    {redSocial: 'Twitter', color: '#1DA1F2'},
+    {redSocial: 'Youtube', color: '#FF0000'}
+  ];
+
+  mostrarHora(evento: any){
+    console.log(evento);
+
+    this.horaActual = new Date();
+
+    const {target} = evento;
+    target.innerHTML = `Cambio la hora - ${this.horaActual.getSeconds()}`;
+  }
+}
+
+interface IColor {
+  redSocial: string;
+  color: string;
 }
